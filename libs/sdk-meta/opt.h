3b83d9cf3321700b032e16626aceda145d494235
xbendan <richardc1ay@outlook.com>
2025-09-29 13:45:32 -0300

    [libs/sdk-meta]: add ref specialized impl to Opt<T>.

--- libs/sdk-meta/opt.h
+++ libs/sdk-meta/opt.h
@@ -8,6 +8,272 @@
 
 namespace Meta {
 
+template <typename>
+struct Opt;
+
+// MARK: - Reference specialization
+
+template <typename T>
+    requires(Meta::Same<Meta::RemoveCvRef<T>&, T>)
+struct Opt<T> {
+    using Inner = Meta::RemoveCvRef<T>*;
+
+    union {
+        Empty _none;
+        Inner _value;
+    };
+    bool _present { false };
+
+    [[gnu::always_inline]] constexpr Opt() : _none(), _present(false) { }
+
+    [[gnu::always_inline]] constexpr Opt(Empty) : _none(), _present(false) { }
+
+    template <typename U = T>
+        requires(not Same<RemoveCvRef<U>, Opt<T>> and Convertible<U, T>)
+    [[gnu::always_inline]] constexpr Opt(U const& value)
+        : _value(&value),
+          _present(true) { }
+
+    template <typename U = Inner>
+        requires(not Same<RemoveCvRef<U>, Opt<T>> and Convertible<U, Inner>)
+    [[gnu::always_inline]] constexpr Opt(U value)
+        : _value(value),
+          _present(true) { }
+
+    // template <typename U = T>
+    //     requires(not Same<RemoveCvRef<U>, Opt<T>> and MoveConstructible<T, U>)
+    // [[gnu::always_inline]] constexpr Opt(U&& value)
+    //     : _value(static_cast<U&&>(value)),
+    //       _present(true) { }
+
+    [[gnu::always_inline]] constexpr Opt(Opt const& other)
+        : _present(other._present) {
+        if (_present)
+            _value = other._value;
+    }
+
+    // [[gnu::always_inline]] constexpr Opt(Opt&& other)
+    //     requires(Meta::MoveConstructible<T>)
+    //     : _present(other._present) {
+    //     if (_present)
+    //         new (&_value) T(Meta::forward<T>(other._value));
+    // }
+
+    template <typename U>
+    [[gnu::always_inline]] constexpr Opt(Opt<U> const& other)
+        requires(Convertible<U, T>)
+        : _present(other._present) {
+        if (_present)
+            new (&_value) T(other.unwrap());
+    }
+
+    // template <typename U>
+    //     requires(Meta::MoveConstructible<T, U>)
+    // [[gnu::always_inline]] constexpr Opt(Opt<U>&& other) : _present(other._present) {
+    //     if (_present)
+    //         new (&_value) T(Meta::forward<U>(other.unwrap()));
+    // }
+
+    ~Opt() { clear(); }
+
+    [[gnu::always_inline]] constexpr Opt& operator=(Empty) {
+        clear();
+        return *this;
+    }
+
+    template <typename U = T>
+        requires(not Meta::Same<Meta::RemoveCvRef<U>, Opt<T>>
+                 and Meta::Convertible<U, T>)
+    [[gnu::always_inline]] constexpr Opt& operator=(U const& value) {
+        clear();
+        _present = true;
+        _value   = &value;
+        return *this;
+    }
+
+    // template <typename U = T>
+    //     requires(not Meta::Same<Meta::RemoveCvRef<U>, Opt<T>>
+    //              and Meta::MoveConstructible<T, U>)
+    // [[gnu::always_inline]] constexpr Opt& operator=(U&& value) {
+    //     clear();
+    //     _present = true;
+    //     new (&_value) T(::forward<U>(value));
+    //     return *this;
+    // }
+
+    [[gnu::always_inline]] constexpr Opt& operator=(Opt const& other) {
+        if (other._present) {
+            _present = true;
+            _value   = other._value;
+        } else {
+            clear();
+        }
+
+        return *this;
+    }
+
+    // [[gnu::always_inline]] constexpr Opt& operator=(Opt&& other)
+    //     requires(Meta::MoveConstructible<T>)
+    // {
+    //     clear();
+    //     if (other._present) {
+    //         _present = true;
+    //         new (&_value) T(Meta::forward<T>(other.take()));
+    //     }
+    //     return *this;
+    // }
+
+    template <typename U = T>
+    [[gnu::always_inline]] constexpr Opt& operator=(Opt<U> const& other)
+        requires(Meta::Convertible<U, T>)
+    {
+        *this = Opt(other);
+        return *this;
+    }
+
+    // template <typename U>
+    // [[gnu::always_inline]] constexpr Opt& operator=(Opt<U>&& other)
+    //     requires(Meta::MoveConstructible<T, U>)
+    // {
+    //     clear();
+    //     if (other._present) {
+    //         _present = true;
+    //         new (&_value) T(::forward<U>(other.take()));
+    //     }
+    //     return *this;
+    // }
+
+    [[gnu::always_inline]] constexpr explicit operator bool() const noexcept {
+        return _present;
+    }
+
+    [[gnu::always_inline]] constexpr Meta::RemoveCvRef<T>* operator->() {
+        if (not _present) [[unlikely]] {
+            panic("Opt::operator-> on Empty");
+        }
+        return _value;
+    }
+
+    [[gnu::always_inline]] constexpr Meta::RemoveCvRef<T> const* operator->()
+        const {
+        if (not _present) [[unlikely]] {
+            panic("Opt::operator->: on Empty");
+        }
+        return _value;
+    }
+
+    [[gnu::always_inline]] constexpr Meta::RemoveCvRef<T>& operator*() {
+        if (not _present) [[unlikely]] {
+            panic("Opt::operator*: on Empty");
+        }
+        return *_value;
+    }
+
+    [[gnu::always_inline]] constexpr Meta::RemoveCvRef<T> const& operator*()
+        const {
+        if (not _present) [[unlikely]] {
+            panic("Opt::operator*: on Empty");
+        }
+        return *_value;
+    }
+
+    // template <typename... Args>
+    // [[gnu::always_inline]] constexpr T& emplace(Args&&... args) {
+    //     if (_present) {
+    //         _value.~T();
+    //     }
+    //     new (&_value) T(Meta::forward<Args>(args)...);
+    //     _present = true;
+    //     return _value;
+    // }
+
+    [[gnu::always_inline]] constexpr void clear() {
+        if (_present) {
+            _value.~Inner();
+            _present = false;
+        }
+    }
+
+    [[gnu::always_inline]] constexpr T& unwrap(char const* msg
+                                               = "Opt::unwrap(): on Empty") {
+        if (not _present) [[unlikely]] {
+            panic(msg);
+        }
+        return *_value;
+    }
+
+    [[gnu::always_inline]] constexpr T const& unwrap(
+        char const* msg = "Opt::unwrap(): unwrap on Empty") const {
+        if (not _present) [[unlikely]] {
+            panic(msg);
+        }
+        return *_value;
+    }
+
+    [[gnu::always_inline]] constexpr T const& unwrapOrElse(T const& def) const {
+        if (_present) {
+            return *_value;
+        }
+        return def;
+    }
+
+    [[gnu::always_inline]] constexpr T unwrapOrElse(
+        Callable<T> auto&& func) const {
+        if (_present) {
+            return *_value;
+        }
+        return func();
+    }
+
+    [[gnu::always_inline]] constexpr T take(char const* msg
+                                            = "Opt::take(): take on Empty") {
+        if (not _present) [[unlikely]] {
+            panic(msg);
+        }
+        // T value = move(_value);
+        // _value.~T();
+        _present = false;
+        return *_value;
+    }
+
+    [[gnu::always_inline]] constexpr auto mapTo(Callable<T> auto&& func)
+        -> Opt<decltype(func(unwrap()))> {
+        if (not _present) {
+            return Empty {};
+        }
+        return func(unwrap());
+    }
+
+    template <typename U>
+    [[gnu::always_inline]] constexpr auto mapTo() -> Opt<U>
+        requires(Meta::Convertible<T, U>)
+    {
+        if (not _present) {
+            return Empty {};
+        }
+        return static_cast<U>(unwrap());
+    }
+
+    // #define mapTo$(expr) mapTo([&](auto x) { return expr; })
+
+    [[gnu::always_inline]] constexpr bool operator==(Empty) const {
+        return not _present;
+    }
+
+    template <typename U>
+        requires Equatable<T, U>
+    [[gnu::always_inline]] constexpr bool operator==(U const& other) const {
+        if (not _present) {
+            return false;
+        }
+        return _value == other;
+    }
+
+    static constexpr Empty none() { return Empty {}; }
+};
+
+// MARK: - General case
+
 template <typename T>
 struct Opt {
 
@@ -19,30 +285,30 @@ struct Opt {
     };
     bool _present { false };
 
-    always_inline constexpr Opt() : _none(), _present(false) { }
+    [[gnu::always_inline]] constexpr Opt() : _none(), _present(false) { }
 
-    always_inline constexpr Opt(Empty) : _none(), _present(false) { }
+    [[gnu::always_inline]] constexpr Opt(Empty) : _none(), _present(false) { }
 
     template <typename U = T>
         requires(not Same<RemoveCvRef<U>, Opt<T>> and CopyConstructible<T, U>)
-    always_inline constexpr Opt(U const& value)
+    [[gnu::always_inline]] constexpr Opt(U const& value)
         : _value(value),
           _present(true) { }
 
     template <typename U = T>
         requires(not Same<RemoveCvRef<U>, Opt<T>> and MoveConstructible<T, U>)
-    always_inline constexpr Opt(U&& value)
+    [[gnu::always_inline]] constexpr Opt(U&& value)
         : _value(static_cast<U&&>(value)),
           _present(true) { }
 
-    always_inline constexpr Opt(Opt const& other)
+    [[gnu::always_inline]] constexpr Opt(Opt const& other)
         requires(Meta::CopyConstructible<T>)
         : _present(other._present) {
         if (_present)
             new (&_value) T(other._value);
     }
 
-    always_inline constexpr Opt(Opt&& other)
+    [[gnu::always_inline]] constexpr Opt(Opt&& other)
         requires(Meta::MoveConstructible<T>)
         : _present(other._present) {
         if (_present)
@@ -50,7 +316,7 @@ struct Opt {
     }
 
     template <typename U>
-    always_inline constexpr Opt(Opt<U> const& other)
+    [[gnu::always_inline]] constexpr Opt(Opt<U> const& other)
         requires(Meta::CopyConstructible<T, U>)
         : _present(other._present) {
         if (_present)
@@ -59,14 +325,15 @@ struct Opt {
 
     template <typename U>
         requires(Meta::MoveConstructible<T, U>)
-    always_inline constexpr Opt(Opt<U>&& other) : _present(other._present) {
+    [[gnu::always_inline]] constexpr Opt(Opt<U>&& other)
+        : _present(other._present) {
         if (_present)
             new (&_value) T(Meta::forward<U>(other.unwrap()));
     }
 
     ~Opt() { clear(); }
 
-    always_inline constexpr Opt& operator=(Empty) {
+    [[gnu::always_inline]] constexpr Opt& operator=(Empty) {
         clear();
         return *this;
     }
@@ -74,7 +341,7 @@ struct Opt {
     template <typename U = T>
         requires(not Meta::Same<Meta::RemoveCvRef<U>, Opt<T>>
                  and Meta::Convertible<U, T>)
-    always_inline constexpr Opt& operator=(U const& value) {
+    [[gnu::always_inline]] constexpr Opt& operator=(U const& value) {
         clear();
         _present = true;
         new (&_value) T(value);
@@ -84,21 +351,21 @@ struct Opt {
     template <typename U = T>
         requires(not Meta::Same<Meta::RemoveCvRef<U>, Opt<T>>
                  and Meta::MoveConstructible<T, U>)
-    always_inline constexpr Opt& operator=(U&& value) {
+    [[gnu::always_inline]] constexpr Opt& operator=(U&& value) {
         clear();
         _present = true;
         new (&_value) T(::forward<U>(value));
         return *this;
     }
 
-    always_inline constexpr Opt& operator=(Opt const& other)
+    [[gnu::always_inline]] constexpr Opt& operator=(Opt const& other)
         requires(Meta::CopyConstructible<T>)
     {
         *this = Opt(other);
         return *this;
     }
 
-    always_inline constexpr Opt& operator=(Opt&& other)
+    [[gnu::always_inline]] constexpr Opt& operator=(Opt&& other)
         requires(Meta::MoveConstructible<T>)
     {
         clear();
@@ -110,7 +377,7 @@ struct Opt {
     }
 
     template <typename U = T>
-    always_inline constexpr Opt& operator=(Opt<U> const& other)
+    [[gnu::always_inline]] constexpr Opt& operator=(Opt<U> const& other)
         requires(Meta::CopyConstructible<T, U>)
     {
         *this = Opt(other);
@@ -118,7 +385,7 @@ struct Opt {
     }
 
     template <typename U>
-    always_inline constexpr Opt& operator=(Opt<U>&& other)
+    [[gnu::always_inline]] constexpr Opt& operator=(Opt<U>&& other)
         requires(Meta::MoveConstructible<T, U>)
     {
         clear();
@@ -129,32 +396,32 @@ struct Opt {
         return *this;
     }
 
-    always_inline constexpr explicit operator bool() const noexcept {
+    [[gnu::always_inline]] constexpr explicit operator bool() const noexcept {
         return _present;
     }
 
-    always_inline constexpr T* operator->() {
+    [[gnu::always_inline]] constexpr T* operator->() {
         if (not _present) [[unlikely]] {
             panic("Opt::operator-> on Empty");
         }
         return &_value;
     }
 
-    always_inline constexpr T const* operator->() const {
+    [[gnu::always_inline]] constexpr T const* operator->() const {
         if (not _present) [[unlikely]] {
             panic("Opt::operator->: on Empty");
         }
         return &_value;
     }
 
-    always_inline constexpr T& operator*() {
+    [[gnu::always_inline]] constexpr T& operator*() {
         if (not _present) [[unlikely]] {
             panic("Opt::operator*: on Empty");
         }
         return _value;
     }
 
-    always_inline constexpr T const& operator*() const {
+    [[gnu::always_inline]] constexpr T const& operator*() const {
         if (not _present) [[unlikely]] {
             panic("Opt::operator*: on Empty");
         }
@@ -162,7 +429,7 @@ struct Opt {
     }
 
     template <typename... Args>
-    always_inline constexpr T& emplace(Args&&... args) {
+    [[gnu::always_inline]] constexpr T& emplace(Args&&... args) {
         if (_present) {
             _value.~T();
         }
@@ -171,45 +438,46 @@ struct Opt {
         return _value;
     }
 
-    always_inline constexpr void clear() {
+    [[gnu::always_inline]] constexpr void clear() {
         if (_present) {
             _value.~T();
             _present = false;
         }
     }
 
-    always_inline constexpr T& unwrap(char const* msg
-                                      = "Opt::unwrap(): on Empty") {
+    [[gnu::always_inline]] constexpr T& unwrap(char const* msg
+                                               = "Opt::unwrap(): on Empty") {
         if (not _present) [[unlikely]] {
             panic(msg);
         }
         return _value;
     }
 
-    always_inline constexpr T const&
-    unwrap(char const* msg = "Opt::unwrap(): unwrap on Empty") const {
+    [[gnu::always_inline]] constexpr T const& unwrap(
+        char const* msg = "Opt::unwrap(): unwrap on Empty") const {
         if (not _present) [[unlikely]] {
             panic(msg);
         }
         return _value;
     }
 
-    always_inline constexpr T const& unwrapOrElse(T const& def) const {
+    [[gnu::always_inline]] constexpr T const& unwrapOrElse(T const& def) const {
         if (_present) {
             return _value;
         }
         return def;
     }
 
-    always_inline constexpr T unwrapOrElse(Callable<T> auto&& func) const {
+    [[gnu::always_inline]] constexpr T unwrapOrElse(
+        Callable<T> auto&& func) const {
         if (_present) {
             return _value;
         }
         return func();
     }
 
-    always_inline constexpr T take(char const* msg
-                                   = "Opt::take(): take on Empty") {
+    [[gnu::always_inline]] constexpr T take(char const* msg
+                                            = "Opt::take(): take on Empty") {
         if (not _present) [[unlikely]] {
             panic(msg);
         }
@@ -219,7 +487,7 @@ struct Opt {
         return value;
     }
 
-    always_inline constexpr auto mapTo(Callable<T> auto&& func)
+    [[gnu::always_inline]] constexpr auto mapTo(Callable<T> auto&& func)
         -> Opt<decltype(func(unwrap()))> {
         if (not _present) {
             return Empty {};
@@ -227,15 +495,25 @@ struct Opt {
         return func(unwrap());
     }
 
+    template <typename U>
+    [[gnu::always_inline]] constexpr auto mapTo() -> Opt<U>
+        requires(Meta::Convertible<T, U>)
+    {
+        if (not _present) {
+            return Empty {};
+        }
+        return static_cast<U>(unwrap());
+    }
+
 #define mapTo$(expr) mapTo([&](auto x) { return expr; })
 
-    always_inline constexpr bool operator==(Empty) const {
+    [[gnu::always_inline]] constexpr bool operator==(Empty) const {
         return not _present;
     }
 
     template <typename U>
         requires Equatable<T, U>
-    always_inline constexpr bool operator==(U const& other) const {
+    [[gnu::always_inline]] constexpr bool operator==(U const& other) const {
         if (not _present) {
             return false;
         }
