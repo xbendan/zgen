545cb178a707a219cf6e9c1158408b7fda4bea68
xbendan <richardc1ay@outlook.com>
2025-10-14 20:25:56 -0300

    [libs/sdk-io]: add id gen and simd header files.

---
+++ libs/sdk-math/gen.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <sdk-meta/limits.h>
+#include <sdk-meta/traits.h>
+#include <sdk-meta/types.h>
+
+namespace Sdk::Math {
+
+template <Meta::Integral T>
+struct RandomIdGen {
+    u64 _seed;
+    T   _cur = Limits<T>::MIN;
+};
+
+struct DiscreteIdGen { };
+
+template <Meta::Integral T>
+struct LoopIdGen {
+    T _cur = Limits<T>::MIN;
+
+    Res<T> next(T limit = Limits<T>::max()) {
+        if (_cur == Limits<T>::MAX) {
+            _cur = Limits<T>::MIN;
+        }
+        return (_cur++ % limit);
+    }
+};
+
+} // namespace Sdk::Math
